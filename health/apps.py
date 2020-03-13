35590a0c2746b3acff2e3ce5f8edaf333ce1921f
Toàn <quachphuongtoanc2@gmail.com>
2020-03-14 00:52:44 +0700

    hoàn thiện bootstrap template

---
+++ health/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HeathConfig(AppConfig):
+    name = 'heath'
--- nienluan/settings.py
+++ nienluan/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'health',
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+MEDIA_URL =  '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]   
